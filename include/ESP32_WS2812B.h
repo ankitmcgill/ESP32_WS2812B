92159ab4fdc1057fdaf52343e91eac997a05912b
Ankit Bhatnagar <ankitmcgill@users.noreply.github.com>
2018-04-30 15:23:23 +0530

    initial file committ

---
+++ include/ESP32_WS2812B.h
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////
+// ESP32 - WS2812B
+//
+// WS2812B RGB LED SRIVER
+// USES ESP32 RMT (REMOTE CONTROL) PERIPHERAL TO GENERATE NRZ SIGNAL
+//
+// NOTE : FROM ESP-IDF MANUAL
+//        http://esp-idf.readthedocs.io/en/latest/api-reference/peripherals/rmt.html?highlight=rmt
+//          
+//        NEIL KOLBAN ESP32 BOOK
+//        SECTION = RMT
+//
+// APRIL 30, 2018
+//
+// ANKIT BHATNAGAR
+// ANKIT.BHATNAGARINDIA@GMAIL.COM
+//////////////////////////////////////////////////////////////////
+
+#ifndef _ESP32_WS2812B_
+#define _ESP32_WS2812B_
+
+#include "esp_system.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+#define ESP32_WS2812B_TAG       "ESP32:WS2812B"
+
+#endif
